215241ce98498c578b4d40888b5bbfe60628fcd5
Benjamin <baboone@byu.edu>
2026-01-12 17:34:06 -0700

    Figured out how to/what to return for bishop moves.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,7 +12,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +36,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,9 +53,24 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         //Todo
         //Temp need to return list of moves not an empty one
+        // I belive that this is where each move will be coded. I might make functions to call for each type so the code is easyer to reed
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            //Todo don't hard code this in
+            List<ChessMove> BishopMoves = new ArrayList<>();
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
+            int drow= 8-row;
+            int dcol = 8-col;
+            BishopMoves.add(new ChessMove( new ChessPosition(5,4),new ChessPosition(1,8), null));
+
+
+            return BishopMoves;
+            }
         return List.of();
     }
 }
