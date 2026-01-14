5a14505150a13efc20dda81ac57efe1890d09d82
Benjamin <baboone@byu.edu>
2026-01-14 15:39:33 -0700

    Implemented constructor

--- shared/src/main/java/chess/MoveCalculator.java
+++ shared/src/main/java/chess/MoveCalculator.java
@@ -9,30 +9,35 @@ import java.util.List;
 
 public class MoveCalculator {
     private final List<ChessMove> movesList = new ArrayList<>();
-    //Todo create a constructor? so the varables i need can be passed in
+
     //Todo quit recalculating the piece and passing the board
     //Todo figure out promotion and side capturing for pawn
 
+    public ChessBoard board;
+    public ChessPosition myPosition;
 
+    public MoveCalculator(ChessBoard board, ChessPosition myPosition) {
+        this.board = board;
+        this.myPosition = myPosition;
+    }
 
-
-    public Collection<ChessMove> pieceCalc(ChessBoard board, ChessPosition myPosition) {
+    public Collection<ChessMove> pieceCalc() {
 
 
         ChessPiece piece = board.getPiece(myPosition);
         if (piece.getPieceType() == ChessPiece.PieceType.BISHOP) {
-            return bishopCalc(myPosition,board);
+            return bishopCalc();
 
         } else if (piece.getPieceType() == ChessPiece.PieceType.ROOK) {
-            return rookCalc(myPosition,board);
+            return rookCalc();
         } else if (piece.getPieceType() == ChessPiece.PieceType.QUEEN) {
-            return queenCalc(myPosition,board);
+            return queenCalc();
         } else if (piece.getPieceType() == ChessPiece.PieceType.KING) {
-            return kingCalc(myPosition,board);
+            return kingCalc();
         } else if ((piece.getPieceType() == ChessPiece.PieceType.KNIGHT)) {
-            return knightCalc(myPosition,board);
+            return knightCalc();
         } else if ((piece.getPieceType() == ChessPiece.PieceType.PAWN)) {
-            return pawnCalc(myPosition,board,piece.getTeamColor());
+            return pawnCalc(piece.getTeamColor());
 
         }
         throw new RuntimeException("Not a valid type");
@@ -45,7 +50,7 @@ public class MoveCalculator {
 
     }
 
-    private  boolean validMoveCheck(int row, int col, int rowN, int colN, ChessBoard board) {
+    private  boolean validMoveCheck(int row, int col, int rowN, int colN) {
         if (inbounds(rowN, colN)) {
             System.out.printf("Inbounds %d,%d ", rowN, colN);
 
@@ -67,7 +72,7 @@ public class MoveCalculator {
     }
 
 
-    private Collection<ChessMove> bishopCalc(ChessPosition myPosition, ChessBoard board) {
+    private Collection<ChessMove> bishopCalc() {
 
 
         int row = myPosition.getRow();
@@ -80,7 +85,7 @@ public class MoveCalculator {
         while (validMove) {
             rowN = rowN - 1;
             colN = colN + 1;
-            validMove = validMoveCheck(row, col, rowN, colN, board);
+            validMove = validMoveCheck(row, col, rowN, colN);
         }
             //upright
             validMove = true;
@@ -89,7 +94,7 @@ public class MoveCalculator {
             while (validMove) {
                 rowN = rowN + 1;
                 colN = colN + 1;
-                validMove = validMoveCheck(row, col, rowN, colN, board);
+                validMove = validMoveCheck(row, col, rowN, colN);
             }
             //downright
             validMove = true;
@@ -98,7 +103,7 @@ public class MoveCalculator {
             while (validMove) {
                 rowN = rowN + 1;
                 colN = colN - 1;
-                validMove = validMoveCheck(row, col, rowN, colN, board);
+                validMove = validMoveCheck(row, col, rowN, colN);
             }
             //downLeft
             validMove = true;
@@ -107,14 +112,14 @@ public class MoveCalculator {
             while (validMove) {
                 rowN = rowN - 1;
                 colN = colN - 1;
-                validMove = validMoveCheck(row, col, rowN, colN, board);
+                validMove = validMoveCheck(row, col, rowN, colN);
             }
 
 
             return movesList;
         }
 
-    private Collection<ChessMove> rookCalc(ChessPosition myPosition, ChessBoard board) {
+    private Collection<ChessMove> rookCalc() {
 
 
         int row = myPosition.getRow();
@@ -126,42 +131,42 @@ public class MoveCalculator {
         int colN = col;
         while (validMove) {
             colN = colN + 1;
-            validMove = validMoveCheck(row, col, rowN, colN, board);
+            validMove = validMoveCheck(row, col, rowN, colN);
         }
         //Right
         validMove = true;
         colN = col;
         while (validMove) {
             rowN = rowN + 1;
-            validMove = validMoveCheck(row, col, rowN, colN, board);
+            validMove = validMoveCheck(row, col, rowN, colN);
         }
         //Down
         validMove = true;
         rowN = row;
         while (validMove) {
             colN = colN - 1;
-            validMove = validMoveCheck(row, col, rowN, colN, board);
+            validMove = validMoveCheck(row, col, rowN, colN);
         }
         //Left
         validMove = true;
         colN = col;
         while (validMove) {
             rowN = rowN - 1;
-            validMove = validMoveCheck(row, col, rowN, colN, board);
+            validMove = validMoveCheck(row, col, rowN, colN);
         }
 
 
         return movesList;
     }
-    private Collection<ChessMove> queenCalc(ChessPosition myPosition, ChessBoard board) {
+    private Collection<ChessMove> queenCalc() {
 
-        bishopCalc(myPosition, board);
-        rookCalc(myPosition, board);
+        bishopCalc();
+        rookCalc();
 
         return movesList;
     }
 
-    private Collection<ChessMove> kingCalc(ChessPosition myPosition, ChessBoard board) {
+    private Collection<ChessMove> kingCalc() {
 
 
         int row = myPosition.getRow();
@@ -174,39 +179,39 @@ public class MoveCalculator {
         //left up
         rowN = row - 1;
         colN = col + 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
 
         //upright
         rowN = row + 1;
         colN = col + 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         //downright
         rowN = row + 1;
         colN = col - 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         //downLeft
         rowN = row - 1;
         colN = col - 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
 
         //left
         rowN = row - 1;
         colN = col;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         //up
         rowN = row;
         colN = col + 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         //down
         colN = col - 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         //Right
         rowN = row + 1;
         colN = col;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         return movesList;
     }
-    private Collection<ChessMove> knightCalc(ChessPosition myPosition, ChessBoard board) {
+    private Collection<ChessMove> knightCalc() {
 
 
         int row = myPosition.getRow();
@@ -219,43 +224,43 @@ public class MoveCalculator {
         //upLeft
         rowN = row + 2;
         colN = col - 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
 
         //upRight
         rowN = row + 2;
         colN = col + 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
 
         //rightUp
         rowN = row + 1;
         colN = col + 2;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         //rightDown
         rowN = row - 1;
         colN = col + 2;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
 
         //downRight
         rowN = row - 2;
         colN = col + 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         //downLeft
         rowN = row - 2;
         colN = col - 1;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         //leftDown
         rowN = row - 1;
         colN = col - 2;
 
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         //leftUp
         rowN = row + 1;
         colN = col - 2;
-        validMoveCheck(row, col, rowN, colN, board);
+        validMoveCheck(row, col, rowN, colN);
         return movesList;
     }
 
-    private Collection<ChessMove> pawnCalc(ChessPosition myPosition, ChessBoard board, ChessGame.TeamColor teamColor) {
+    private Collection<ChessMove> pawnCalc( ChessGame.TeamColor teamColor) {
 
 
         int row = myPosition.getRow();
@@ -266,20 +271,20 @@ public class MoveCalculator {
             System.out.println("This pawn is on team white");
             //forwardWhite
             rowN = row + 1;
-            validMoveCheck(row, col, rowN, colN, board);
+            validMoveCheck(row, col, rowN, colN);
             if (row == 2) {
                 rowN = row + 2;
-                validMoveCheck(row, col, rowN, colN, board);
+                validMoveCheck(row, col, rowN, colN);
             }
         }
         if (teamColor == ChessGame.TeamColor.BLACK) {
             System.out.println("This pawn is on team Black");
             //forwardBlack
             rowN = row - 1;
-            validMoveCheck(row, col, rowN, colN, board);
+            validMoveCheck(row, col, rowN, colN);
             if (row == 7) {
                 rowN = row - 2;
-                validMoveCheck(row, col, rowN, colN, board);
+                validMoveCheck(row, col, rowN, colN);
             }
         }
         return movesList;
