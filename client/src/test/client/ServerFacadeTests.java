36379bca94ea6a1006dbc313a1e0ce8a6843d650
Benjamin <baboone@byu.edu>
2026-03-19 15:18:10 -0600

    Added more test cases like list and create

--- client/src/test/client/ServerFacadeTests.java
+++ client/src/test/client/ServerFacadeTests.java
@@ -58,12 +58,7 @@ public class ServerFacadeTests {
 
     @Test
     void logout() throws Exception{
-        Request.RegisterRequest rRequest = new Request.RegisterRequest();
-        rRequest.setUsername("player1");
-        rRequest.setPassword("password");
-        rRequest.setEmail("p1@email.com");
-        Response.RegisterResponse response = facade.registerUser(rRequest);
-        String token = response.getAuthToken();
+        String token = createUser("player1","password","email");
 
         Request.LogoutRequest request = new Request.LogoutRequest(token);
         assertDoesNotThrow(()-> facade.logoutUser(request, token));
@@ -71,15 +66,110 @@ public class ServerFacadeTests {
     }
     @Test
     void logoutBadToken() throws Exception{
-        Request.RegisterRequest rRequest = new Request.RegisterRequest();
-        rRequest.setUsername("player1");
-        rRequest.setPassword("password");
-        rRequest.setEmail("p1@email.com");
-        Response.RegisterResponse response = facade.registerUser(rRequest);
-        String token = response.getAuthToken();
+        String token = createUser("player1","password","email");
+
 
         Request.LogoutRequest request = new Request.LogoutRequest("token");
         assertThrows(Exception.class,()-> facade.logoutUser(request, "token"));
 
     }
+
+    @Test
+    void login() throws Exception{
+        //set up
+
+        String token = createUser("player1","password","email");
+
+        Request.LogoutRequest lRequest = new Request.LogoutRequest(token);
+        facade.logoutUser(lRequest, token);
+
+        //login
+        Request.LoginRequest request = new Request.LoginRequest();
+        request.setUsername("player1");
+        request.setPassword("password");
+        Response.LoginResponse response = facade.loginUser(request);
+        assertNotEquals(token,response.getAuthToken());
+
+    }
+
+    @Test
+    void loginBadUsername() throws Exception{
+        //set up
+        String token = createUser("player1","password","email");
+
+        //login
+        Request.LoginRequest request = new Request.LoginRequest();
+        request.setUsername("bad");
+        request.setPassword("password");
+        assertThrows(Exception.class , ()-> facade.loginUser(request));
+
+
+    }
+
+    @Test
+    void createGame()throws Exception{
+        Request.CreateGameRequest request = new Request.CreateGameRequest();
+        request.setGameName("gameName");
+        String token = createUser("Username","Password","Email");
+        assertDoesNotThrow(()->facade.createGame(request, token));
+
+    }
+    @Test
+    void createGameBadAuth()throws Exception{
+        Request.CreateGameRequest request = new Request.CreateGameRequest();
+        request.setGameName("gameName");
+        createUser("Username","Password","Email");
+        assertThrows(Exception.class,()->facade.createGame(request, "token"));
+    }
+
+
+    @Test
+    void listGames() throws Exception{
+        Request.CreateGameRequest cRequest = new Request.CreateGameRequest();
+        cRequest.setGameName("gameName");
+        String token = createUser("Username","Password","Email");
+        facade.createGame(cRequest, token);
+        Request.CreateGameRequest cRequest2 = new Request.CreateGameRequest();
+        cRequest2.setGameName("gameName2");
+        facade.createGame(cRequest2, token);
+
+        //listGames
+        Request.ListGamesRequest request = new Request.ListGamesRequest();
+        Response.ListGamesResponse response = facade.listGames(request, token);
+        assertEquals(2, response.getGames().size());
+    }
+    @Test
+    void listGamesBadToken() throws Exception{
+        Request.CreateGameRequest cRequest = new Request.CreateGameRequest();
+        cRequest.setGameName("gameName");
+        String token = createUser("Username","Password","Email");
+        facade.createGame(cRequest, token);
+        Request.CreateGameRequest cRequest2 = new Request.CreateGameRequest();
+        cRequest2.setGameName("gameName2");
+        facade.createGame(cRequest2, token);
+
+        //listGames
+        Request.ListGamesRequest request = new Request.ListGamesRequest();
+        assertThrows(Exception.class,()->facade.listGames(request, "BadToken"));
+
+    }
+
+
+
+
+
+
+
+
+
+
+    String createUser(String username,String password, String email) throws Exception {
+        Request.RegisterRequest rRequest = new Request.RegisterRequest();
+        rRequest.setUsername(username);
+        rRequest.setPassword(password);
+        rRequest.setEmail(email);
+        Response.RegisterResponse lResponse = facade.registerUser(rRequest);
+        return lResponse.getAuthToken();
+    }
 }
+
